da5bbcec9542394108a099d56c31b35b6dad7167
Phase1es2 <hyang17@uw.edu>
2025-05-09 23:44:11 -0400

    setup the volume, source, target, hostip for yaml

---
+++ script/setup_host_ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This code is to set up the HOST_IP
+# so that we can set the tracking in jupyter environment.
+
+echo "Set up HOST_IP..."
+export HOST_IP=129.114.24.214
+echo $HOST_IP
